806926877c4f258780aeb7f9650de5ce559a259e
Faissal Bensefia <faissal@madetech.com>
2020-12-09 16:31:56 +0000

    Refactor

--- calculator.js
+++ calculator.js
@@ -1,20 +1,16 @@
 var calculate = function(corn, geese, foxes) {
     let worker = [];
 
-    if((corn != 0) && (geese != 0)) {
-        if(corn==1&&geese==1) {
+    if ((corn != 0) && (geese != 0)) {
+        if ((corn==1) && (geese==1)) {
             return ["corn", "", "goose"];
-        }
-
-        else if(corn==1&&geese==2) {
+        } else if ((corn==1) && (geese==2)) {
             return ["corn", "", "goose", "corn", "goose", "", "corn"];
-        }
-
-        else if(corn==2&&geese==1) {
+        } else if ((corn==2) && (geese==1)) {
             return ["goose", "", "corn", "goose", "corn", "", "goose"];
+        } else {
+          return "ERROR";
         }
-
-        else return "ERROR";
     } else {
         while (corn--) {
             worker.push("corn");
